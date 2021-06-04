8ae16d487da5f2f085e1e11e7ea31e1e5729ce93
adrian4448 <adrian.morais4448@gmail.com>
2021-06-04 17:35:16 -0300

    Adicionando entity post

---
+++ src/main/java/com/adrianmorais/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.adrianmorais.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adrianmorais.workshopmongo.domain.Post;
+import com.adrianmorais.workshopmongo.domain.User;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
